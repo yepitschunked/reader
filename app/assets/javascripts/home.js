23d50b75d532737e16f50aafd1c2b855bfb5db05
Victor Lin <victorhlin@gmail.com>
2011-12-22 01:46:45 -0800

    tuning scrolling parameters

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -45,11 +45,15 @@ $(function() {
 
   var didScroll = false;
   var scrollTimer = null;
+  var scrollPrev = window.pageYOffset;
+  var scrollDirection = 0; // 0 for up, 1 for down
 
   $(window).scroll(function() {
     clearTimeout(scrollTimer);
     scrollTimer = setTimeout(function() {
       didScroll = true;
+      scrollDirection = (scrollPrev > window.pageYOffset) ? 0 : 1;
+      scrollPrev = window.pageYOffset;
     }, 150);
   });
 
@@ -58,14 +62,14 @@ $(function() {
       didScroll = false;
       // Check your page position and then
       // Load in more results
-      var scroll_padding = 20;
+      var scroll_padding = scrollDirection ? -0.2 : 0.55;
       var items = $('.item');
       if(items.first().offset().top > window.pageYOffset) {
         items.first().trigger('read').trigger('focus');
       }
       else {
         $('.item').each(function() {
-          if($(this).offset().top - window.pageYOffset < $(this).prev().height()*0.33 ) {
+          if(window.pageYOffset > $(this).offset().top - $(this).prev().outerHeight()*0.55 ) {
             $(this).trigger('read').trigger('focus');
           }
         });
