6dba8d81dda24beb36b770fb4f3f9600ebef5fce
Victor Lin <victorhlin@gmail.com>
2011-12-22 02:47:42 -0800

    dont crash with empty feeds

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    session[:active_feed] ||= current_user.feeds.first.id
+    session[:active_feed] ||= current_user.feeds.first.try(:id)
   end
 end
