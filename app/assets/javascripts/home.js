efc24d1f495822a5bf07a8bf7f72878cc56c3aea
Victor Lin <victorhlin@gmail.com>
2011-12-22 01:34:59 -0800

    mark as read as you scroll

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -14,12 +14,25 @@ $(function() {
       }
       $('.content').append(entry);
     });
+    $('.content').find('.item').first().attr('id', 'viewing');
 
     $('#sidebar li.subscription.selected').removeClass('selected');
     $('#feed_'+json.id).addClass('selected');
   });
 
-  $('.item:not(.read)').live('click', function() {
+  $('.item:not(.read)').live({
+    'click': function() {
+      $(this).trigger('read');
+      $(this).trigger('focus');
+    },
+  });
+
+  $('.item').live('focus', function() {
+      $('#viewing').removeAttr('id');
+      $(this).attr('id', 'viewing');
+  });
+
+  $('.item:not(.read)').live('read', function() {
     $.ajax({
       url: '/items/'+$(this).data('item-id')+'/read',
       type: 'put',
@@ -29,5 +42,35 @@ $(function() {
       }
     });
   });
+
+  var didScroll = false;
+  var scrollTimer = null;
+
+  $(window).scroll(function() {
+    clearTimeout(scrollTimer);
+    scrollTimer = setTimeout(function() {
+      didScroll = true;
+    }, 150);
+  });
+
+  setInterval(function() {
+    if ( didScroll ) {
+      didScroll = false;
+      // Check your page position and then
+      // Load in more results
+      var scroll_padding = 20;
+      var items = $('.item');
+      if(items.first().offset().top > window.pageYOffset) {
+        items.first().trigger('read').trigger('focus');
+      }
+      else {
+        $('.item').each(function() {
+          if($(this).offset().top - window.pageYOffset < $(this).prev().height()*0.33 ) {
+            $(this).trigger('read').trigger('focus');
+          }
+        });
+      }
+    }
+  }, 250);
 });
 
