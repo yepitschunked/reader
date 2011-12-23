d99b1eb49e263dcfc6cafe67eb63deecef3ab07f
Victor Lin <victorhlin@gmail.com>
2011-12-22 17:50:42 -0800

    Rails.root is a pathname object, not a string

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -20,7 +20,7 @@ after_fork do |server, worker|
 end
 
 before_fork do |server, worker|
-  old_pid = Rails.root + '/tmp/pids/unicorn.pid.oldbin'
+  old_pid = Rails.root.to_s + '/tmp/pids/unicorn.pid.oldbin'
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
