ed523a346f7988a349ee06dfeb3e7ad01468dee8
Victor Lin <victorhlin@gmail.com>
2011-12-23 17:07:33 -0800

    fix test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111222041003) do
+ActiveRecord::Schema.define(:version => 20111222055246) do
 
   create_table "comments", :force => true do |t|
     t.text     "content"
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,7 +9,7 @@ describe Item do
     it "should copy over title, content, url, and item id" do
       item = Item.build_from_feed_item(@feed, @feed_item)
       item.title.should == @feed_item.title
-      item.content.should == @feed_item.content
+      item.content.strip.should == @feed_item.content.strip
       item.original_location.should == @feed_item.url
       item.item_id.should == Digest::MD5.hexdigest(@feed_item.url)
     end
